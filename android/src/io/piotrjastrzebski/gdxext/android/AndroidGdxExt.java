759a1986879f1f27be3becf4a81a27c4fdf8e2f8
piotr-j <me@piotrjastrzebski.io>
2016-07-15 16:15:57 +0200

    android aar dependency test

--- android/src/io/piotrjastrzebski/gdxext/android/AndroidGdxExt.java
+++ android/src/io/piotrjastrzebski/gdxext/android/AndroidGdxExt.java
@@ -1,5 +1,6 @@
 package io.piotrjastrzebski.gdxext.android;
 
+import io.piotrjastrzebski.aartestlib.AARTest;
 import io.piotrjastrzebski.gdxext.core.GdxExt;
 import io.piotrjastrzebski.gdxext.core.GdxExtManager;
 
@@ -7,11 +8,31 @@ import io.piotrjastrzebski.gdxext.core.GdxExtManager;
  * Created by PiotrJ on 01/07/16.
  */
 public class AndroidGdxExt implements GdxExt {
+	/*
+	This libs code is:
+	public class AARTest {
+	 public float compute (float x, float y) {
+		  return x + y;
+	 }
+
+	 public String type () {
+		  return "AAR";
+	 }
+	}
+
+	aar was generated with buildDebug in android studio, lib stub was generated via new module, android library,
+
+	 */
+	AARTest aarTest;
+	public AndroidGdxExt() {
+		aarTest = new AARTest();
+	}
+
 	@Override public float compute (float x, float y) {
-		return x + y;
+		return aarTest.compute(x, y);
 	}
 
 	@Override public String type () {
-		return "Android";
+		return aarTest.type();
 	}
 }
